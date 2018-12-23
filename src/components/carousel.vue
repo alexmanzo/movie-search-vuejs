2b16f8bf8e518d62a4250d4e2de9044acc4d1a02
alexmanzo <memanzo17@gmail.com>
2018-12-23 01:28:31 -0500

    Worked on carousel functionality

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -2,35 +2,57 @@
   <div class="carousel-wrapper">
     <div class="carousel--nav__left" @click="moveCarousel(-1)" :disabled="atHeadOfList"></div>
     <div class="carousel" ref="carousel">
-      <div class="carousel--overflow-container">
-        <div class="carousel-items" :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}" >
-          <div ref="card" class="carousel--card" v-for="(item, index) in items" :key="index">
-            <div v-if="item.site === 'YouTube'" class="video-container">
-                <iframe 
-                    :title="`${item.name}`"
-                    id="ytplayer"
-                    type="text/html"
-                    :src= "`https://www.youtube.com/embed/${item.key}`"
-                    frameBorder="0">
-                    </iframe>
-                    <p>{{ item.name }}</p>
-            </div>
-            <div v-else>
-                <router-link :to="{ name: routerName, params: { id:  item.id, name: item.name  }}">
-                <img v-if="item.profile_path === null || item.poster_path === null" id="no-photo" :src="noPhoto" alt='no profile found' />
-                <img v-else-if="!item.profile_path" :src="`https://image.tmdb.org/t/p/w1280/${item.poster_path}`" :alt="`${item.original_title}`" />
-                <img v-else :src="`https://image.tmdb.org/t/p/w1280/${item.profile_path}`" :alt="`${item.name}`" />
-                <div v-if="!item.name" class="carousel--card--footer">
-                    <p><strong>{{ item.original_title }}  ({{ item.release_date.substring(0,4) }})</strong></p>
-                </div>
-                <div v-else class="carousel--card--footer">
-                    <p><strong>{{ item.name }}</strong></p>
-                    <p>{{ item.character }}</p>
-                </div>
-            </router-link>
-            </div>
+      <div
+        class="carousel-items"
+        :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}"
+      >
+        <div
+          ref="card"
+          class="carousel--card"
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <div v-if="item.site === 'YouTube'" class="video-container">
+            <iframe
+              :title="`${item.name}`"
+              id="ytplayer"
+              type="text/html"
+              :src="`https://www.youtube.com/embed/${item.key}`"
+              frameborder="0"
+            ></iframe>
+            <p>{{ item.name }}</p>
           </div>
-          
+          <div v-else>
+            <router-link :to="{ name: routerName, params: { id:  item.id, name: item.name  }}">
+              <img
+                v-if="item.profile_path === null || item.poster_path === null"
+                id="no-photo"
+                :src="noPhoto"
+                alt="no profile found"
+              >
+              <img
+                v-else-if="!item.profile_path"
+                :src="`https://image.tmdb.org/t/p/w1280/${item.poster_path}`"
+                :alt="`${item.original_title}`"
+              >
+              <img
+                v-else
+                :src="`https://image.tmdb.org/t/p/w1280/${item.profile_path}`"
+                :alt="`${item.name}`"
+              >
+              <div v-if="!item.name" class="carousel--card--footer">
+                <p>
+                  <strong>{{ item.original_title }} ({{ item.release_date.substring(0,4) }})</strong>
+                </p>
+              </div>
+              <div v-else class="carousel--card--footer">
+                <p>
+                  <strong>{{ item.name }}</strong>
+                </p>
+                <p>{{ item.character }}</p>
+              </div>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -39,42 +61,42 @@
 </template>
 
 <script>
-import noPhoto from '@/assets/nophoto.svg'
+import noPhoto from "@/assets/nophoto.svg";
 export default {
-    props: {
-        items: {
-            type: Array,
-            required: true
-        },
-        routerName: {
-            type: String,
-            required: true
-        }
-
+  props: {
+    items: {
+      type: Array,
+      required: true
     },
+    routerName: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {
       noPhoto,
       currentOffset: 0,
       windowSize: 5,
       paginationFactor: 200,
-      numOfCards: 4
-    }
+      numOfCards: 4,
+      overflow: false
+    };
   },
   computed: {
     atEndOfList() {
       return (
-        this.currentOffset <= 
+        this.currentOffset <=
         this.paginationFactor * -1 * (this.items.length - this.windowSize)
-      )
+      );
     },
     atHeadOfList() {
-      return this.currentOffset === 0
-    }
+      return this.currentOffset === 0;
+    },
   },
   methods: {
     moveCarousel(direction) {
-      this.setPagination()
+      this.setPagination();
       if (direction === 1 && !this.atEndOfList) {
         this.currentOffset -= this.paginationFactor;
       } else if (direction === -1 && !this.atHeadOfList) {
@@ -85,28 +107,27 @@ export default {
       this.paginationFactor = this.$refs.card[0].clientWidth + 20
     }
   }
-}
-
+};
 </script>
 
 <style lang="scss">
 @import "main.scss";
 .carousel-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .carousel {
-  display: flex;
   justify-content: center;
-  width: 90%;
+  width: 100%;
   height: 40vh;
-  
+  overflow: hidden;
+
   &--overflow-container {
     overflow: hidden;
   }
-  
+
   &--nav__left,
   &--nav__right {
     display: inline-block;
@@ -120,14 +141,14 @@ export default {
       cursor: auto;
     }
   }
-  
+
   &--nav__left {
     transform: rotate(-135deg);
     &:active {
       transform: rotate(-135deg) scale(0.9);
     }
   }
-  
+
   &--nav__right {
     transform: rotate(45deg);
     &:active {
@@ -141,37 +162,36 @@ export default {
   transition: transform 1000ms ease;
 
   a {
-        text-decoration: none;
-        color: $white;
+    text-decoration: none;
+    color: $white;
 
-         &:hover {
-            color: $green;
-        }
+    &:hover {
+      color: $green;
     }
- 
+  }
+
   .carousel--card {
-    margin: 0 10px;
     cursor: pointer;
-    margin-bottom: 2px;
-    
+    margin: 0 10px;
+
     &:first-child {
       margin-left: 0;
     }
-    
+
     &:last-child {
       margin-right: 0;
     }
-    
+
     img {
-      height: 30vh;  
-      border-radius: 4px;                                      
+      height: 30vh;
+      border-radius: 4px;
       vertical-align: bottom;
-      
+
       &:hover {
         filter: brightness(110%);
       }
     }
-    
+
     &--footer {
       padding: 7px 15px;
 
@@ -188,5 +208,4 @@ export default {
     }
   }
 }
-
 </style>
